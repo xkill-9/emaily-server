299867eb4207f46e1e2cf2168e1c7a13cf84b255
Jorge Morales <jorge.iv.morales@gmail.com>
2019-04-30 00:22:49 -0500

    Basic passport setup, added workspace color, added env variable example and local setup

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  port: process.env.PORT || 5000,
+};
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const { googleClientID, googleClientSecret, port } = require('./config');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hello: 'there!' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {}
+  )
+);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+app.listen(port);
